a5d85598b4464a86bead548ceef5660af8a06b0a
plamengeorgiev <plamen.georgiev@mentormatel.com>
2020-05-08 18:53:36 +0300

    Added backend and finished stripe payment functionality

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_dDrBcMUVY6TkSWOr95ak7at000bVdUiTPv';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful');
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Error. Check the credit card number');
+      });
   };
 
   return (
